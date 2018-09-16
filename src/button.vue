51c380fad46ba93af9c77732b7fdfb22fddcc5f0
spark <jschenfan@qq.com>
2018-09-16 18:08:59 +0800

    button的创建

---
+++ src/button.vue
@@ -0,0 +1,80 @@
+<template>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+<script>
+    export default {
+        props: {
+            icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right';
+                }
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg)
+        }
+        100% {
+            transform: rotate(360deg)
+        }
+    }
+
+    .g-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        > .icon {
+            order: 1;
+            margin-right: .3em
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content {
+                order: 1
+            }
+        }
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        .loading{
+            animation: spin 2s infinite linear;
+        }
+    }
+
+
+</style>
